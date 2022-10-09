d62c56e1f864ed183c3105e55b38ebe8eb0ce131
MimicCAVC <697977@student.cavc.ac.uk>
2022-10-09 16:12:40 +0100

    Yes

--- Assets/Scripts/DriftEnable.cs
+++ Assets/Scripts/DriftEnable.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class DriftEnable : MonoBehaviour
 {
+    WheelCollider wheelCol;
     // Start is called before the first frame update
     void Start()
     {
-        
+        wheelCol.GetComponent<WheelCollider>();
     }
 
     // Update is called once per frame
