e14d6952679788ba2aae40d86fbb2b84fc6d4c26
MimicCAVC <697977@student.cavc.ac.uk>
2022-10-09 17:28:48 +0100

    DriftBoi

--- Assets/Scripts/DriftEnable.cs
+++ Assets/Scripts/DriftEnable.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class DriftEnable : MonoBehaviour
 {
-    WheelCollider wheelCol;
+   [SerializeField] WheelCollider wheelCol;
+    WheelFrictionCurve wheelFrictionCurve;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +15,27 @@ public class DriftEnable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            wheelFrictionCurve = wheelCol.sidewaysFriction;
+
+            wheelFrictionCurve.extremumSlip = 1f;
+            wheelFrictionCurve.extremumValue = 1f;
+            wheelFrictionCurve.asymptoteSlip = 1f;
+            wheelFrictionCurve.asymptoteValue = 1f;
+            wheelFrictionCurve.stiffness = 1f;
+
+            wheelCol.sidewaysFriction = wheelFrictionCurve;
+        }
+        else
+        {
+            wheelFrictionCurve.extremumSlip = 3f;
+            wheelFrictionCurve.extremumValue = 3f;
+            wheelFrictionCurve.asymptoteSlip = 3f;
+            wheelFrictionCurve.asymptoteValue = 3f;
+            wheelFrictionCurve.stiffness = 3f;
+
+            wheelCol.sidewaysFriction = wheelFrictionCurve;
+        }
     }
 }
