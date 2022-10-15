74ddb1c9d3d63499bdf8c8d7c25dfcda09069a66
Fatal-Dev <115042556+fatal-developer@users.noreply.github.com>
2022-10-15 06:37:47 +0100

    MULTIPLAYER MENU DONE!

---
+++ Assets/_Scripts/Networking/Launcher.cs
@@ -0,0 +1,144 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+using TMPro;
+
+public class Launcher : MonoBehaviourPunCallbacks
+{
+    #region Singleton
+    public static Launcher instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    [SerializeField] TMP_InputField roomNameInputField;
+    [SerializeField] TMP_InputField usernameInputField;
+    [SerializeField] TMP_Text errorText;
+    [SerializeField] TMP_Text roomNameText;
+    [SerializeField] Transform roomListContent;
+    [SerializeField] GameObject roomListItemPrefab;
+    [SerializeField] Transform playerListContent;
+    [SerializeField] GameObject playerListItemPrefab;
+    [SerializeField] Transform startGameButton;
+
+    void Start()
+    {
+        Debug.Log("Connecting To Master Servers");
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public void SetUsername()
+    {
+        PhotonNetwork.NickName = usernameInputField.text;
+
+        if (usernameInputField.text == "")
+            PhotonNetwork.NickName = "Player#" + Random.Range(0, 9999).ToString("0000");
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connect To Master Servers");
+        PhotonNetwork.JoinLobby();
+        PhotonNetwork.AutomaticallySyncScene = true;
+    }
+
+    public override void OnJoinedLobby()
+    {
+        MenuManager.instance.OpenMenu("title");
+        Debug.Log("Connect To Master Lobby");
+
+        if (usernameInputField.text == "")
+            PhotonNetwork.NickName = "Player#" + Random.Range(0, 9999).ToString("0000");
+    }
+
+    public void CreateRoom()
+    {
+        if (string.IsNullOrEmpty(roomNameInputField.text)) return;
+
+        PhotonNetwork.CreateRoom(roomNameInputField.text);
+
+        MenuManager.instance.OpenMenu("loading");
+    }
+
+    public override void OnJoinedRoom()
+    {
+        MenuManager.instance.OpenMenu("room");
+        roomNameText.text = PhotonNetwork.CurrentRoom.Name;
+
+        Player[] players = PhotonNetwork.PlayerList;
+
+        foreach (Transform child in playerListContent)
+        {
+            Destroy(child.gameObject);
+        }
+
+        for (int i = 0; i < players.Length; i++)
+        {
+            Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(players[i]);
+        }
+
+        startGameButton.gameObject.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        startGameButton.gameObject.SetActive(PhotonNetwork.IsMasterClient);
+    }
+
+    public override void OnCreateRoomFailed(short returnCode, string message)
+    {
+        MenuManager.instance.OpenMenu("error");
+        errorText.text = "Room creation failed: " + message + " return code: " + returnCode;
+    }
+
+    public void StartGame()
+    {
+        PhotonNetwork.LoadLevel(1);
+    }
+
+    public void LeaveRoom()
+    {
+        PhotonNetwork.LeaveRoom();
+        MenuManager.instance.OpenMenu("loading");
+    }
+
+    public void JoinRoom(RoomInfo info)
+    {
+        PhotonNetwork.JoinRoom(info.Name);
+        MenuManager.instance.OpenMenu("loading");
+    }
+
+    public override void OnLeftRoom()
+    {
+        MenuManager.instance.OpenMenu("title");
+    }
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        foreach (Transform trans in roomListContent)
+        {
+            Destroy(trans.gameObject);
+        }
+
+        for (int i = 0; i < roomList.Count; i++)
+        {
+            if (roomList[i].RemovedFromList) continue;
+
+            Instantiate(roomListItemPrefab, roomListContent).GetComponent<RoomListItem>().SetUp(roomList[i]);
+        }
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Instantiate(playerListItemPrefab, playerListContent).GetComponent<PlayerListItem>().SetUp(newPlayer);
+    }
+
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/_Scripts/Networking/UI/Menu/MenuManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    #region Singleton
+    public static MenuManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+    #endregion
+
+    [SerializeField] Menu[] menus;
+
+    public void OpenMenu(string menuName)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].menuName == menuName)
+            {
+                menus[i].Open();
+            }
+            else if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+    }
+
+    public void OpenMenu(Menu menu)
+    {
+        for (int i = 0; i < menus.Length; i++)
+        {
+            if (menus[i].open)
+            {
+                CloseMenu(menus[i]);
+            }
+        }
+
+        menu.Open();
+    }
+
+    public void CloseMenu(Menu menu)
+    {
+        menu.Close();
+    }
+}
